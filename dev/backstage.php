a29b48c7b661b4790ba06322cce5dfd02cb4781d
hsuanman <hsuanmanp@gmail.com>
2020-08-25 00:51:41 +0800

    backstage_0824

--- dev/backstage.php
+++ dev/backstage.php
@@ -15,145 +15,143 @@
     <script src="./js/fa5.14.0all.js"integrity="sha512-YSdqvJoZr83hj76AIVdOcvLWYMWzy6sJyIMic2aQz5kh2bPTd9dzY3NtdeEAzPp/PhgZqr4aJObB3ym/vsItMg=="crossorigin="anonymous"></script>
 </head>
 <body>
-
+<main>
     <?php
         require_once("./BackstageHeader.inc");
     ?>
-
-<main>
     <aside>
-    <nav id="BackstageNav">
-        <ul class="page index ulat"><a href="">
-                <p class="title">首頁</p>
-            </a></ul>
-        <ul class="report">
-            <p class="title">檢舉</p>
-            <li class="page navli tour"><a href="">
-                    <p class="desc">揪團</p>
-                    <div class="note">
-                        <p>
-                            <?php 
-                            try	{
-                                require_once('connectMeetain.php');
-                                if($pdo != false){
-                                        foreach($pdo->query('SELECT COUNT(*) FROM tour_report tr join tour t on tr.tour_report_tour = t.tour_no where tr.tour_report_situation = "未處理"') as $row) {
-                                        echo $row['COUNT(*)'];
-                                        }
+        <nav id="BackstageNav">
+            <ul class="page index ulat"><a href="">
+                    <p class="title">首頁</p>
+                </a></ul>
+            <ul class="report">
+                <p class="title">檢舉</p>
+                <li class="page navli tour"><a href="">
+                        <p class="desc">揪團</p>
+                        <div class="note">
+                            <p>
+                                <?php 
+                                try	{
+                                    require_once('connectMeetain.php');
+                                    if($pdo != false){
+                                            foreach($pdo->query('SELECT COUNT(*) FROM tour_report tr join tour t on tr.tour_report_tour = t.tour_no where tr.tour_report_situation = "未處理"') as $row) {
+                                            echo $row['COUNT(*)'];
+                                            }
 
-                                }	else	{
-                                    echo "<br>失敗ㄌ<br>";
+                                    }	else	{
+                                        echo "<br>失敗ㄌ<br>";
+                                    }
+                                }	catch	(PDOException $e)	{
+                                    echo "錯誤原因：",$e->getMessage(), "<br>";
+                                    echo "錯誤行號：",$e->getLine(),"<br>";
                                 }
-                            }	catch	(PDOException $e)	{
-                                echo "錯誤原因：",$e->getMessage(), "<br>";
-                                echo "錯誤行號：",$e->getLine(),"<br>";
-                            }
-                            ?>
-                        </p>
-                    </div>
-                </a></li>
-            <li class="page navli forum"><a href="">
-                    <p class="desc">討論</p>
-                    <div class="note">
-                        <p>
-                            <?php 
-                            try	{
-                                require_once('connectMeetain.php');
-                                if($pdo != false){
-                                    foreach($pdo->query('SELECT COUNT(*) FROM forum_report fr join forum_post fp on fr.forum_report_post = fp.forum_post_no where fr.forum_report_situation = "未處理"') as $row) {
-                                        echo $row['COUNT(*)'];
-                                        }
+                                ?>
+                            </p>
+                        </div>
+                    </a></li>
+                <li class="page navli forum"><a href="">
+                        <p class="desc">討論</p>
+                        <div class="note">
+                            <p>
+                                <?php 
+                                try	{
+                                    require_once('connectMeetain.php');
+                                    if($pdo != false){
+                                        foreach($pdo->query('SELECT COUNT(*) FROM forum_report fr join forum_post fp on fr.forum_report_post = fp.forum_post_no where fr.forum_report_situation = "未處理"') as $row) {
+                                            echo $row['COUNT(*)'];
+                                            }
 
-                                }	else	{
-                                    echo "<br>失敗ㄌ<br>";
+                                    }	else	{
+                                        echo "<br>失敗ㄌ<br>";
+                                    }
+                                }	catch	(PDOException $e)	{
+                                    echo "錯誤原因：",$e->getMessage(), "<br>";
+                                    echo "錯誤行號：",$e->getLine(),"<br>";
                                 }
-                            }	catch	(PDOException $e)	{
-                                echo "錯誤原因：",$e->getMessage(), "<br>";
-                                echo "錯誤行號：",$e->getLine(),"<br>";
-                            }
-                            ?>
-                        </p>
-                    </div>
-                </a></li>
-            <li class="page navli comment"><a href="">
-                    <p class="desc">留言</p>
-                    <div class="note">
-                        <p>
-                            <?php 
-                            try	{
-                                require_once('connectMeetain.php');
-                                if($pdo != false){
-                                        foreach($pdo->query('SELECT COUNT(*) FROM comment_report cr JOIN comment_post cp ON cr.comment_report_comment = cp.comment_no WHERE cr.comment_report_situation = "未處理"') as $row) {
-                                        echo $row['COUNT(*)'];
-                                        }
+                                ?>
+                            </p>
+                        </div>
+                    </a></li>
+                <li class="page navli comment"><a href="">
+                        <p class="desc">留言</p>
+                        <div class="note">
+                            <p>
+                                <?php 
+                                try	{
+                                    require_once('connectMeetain.php');
+                                    if($pdo != false){
+                                            foreach($pdo->query('SELECT COUNT(*) FROM comment_report cr JOIN comment_post cp ON cr.comment_report_comment = cp.comment_no WHERE cr.comment_report_situation = "未處理"') as $row) {
+                                            echo $row['COUNT(*)'];
+                                            }
 
-                                }	else	{
-                                    echo "<br>失敗ㄌ<br>";
+                                    }	else	{
+                                        echo "<br>失敗ㄌ<br>";
+                                    }
+                                }	catch	(PDOException $e)	{
+                                    echo "錯誤原因：",$e->getMessage(), "<br>";
+                                    echo "錯誤行號：",$e->getLine(),"<br>";
                                 }
-                            }	catch	(PDOException $e)	{
-                                echo "錯誤原因：",$e->getMessage(), "<br>";
-                                echo "錯誤行號：",$e->getLine(),"<br>";
-                            }
-                            ?>
-                        </p>
-                    </div>
-                </a></li>
-        </ul>
-        <ul class="verify">
-            <p class="title">審核</p>
-            <li class="page navli realname"><a href="">
-                    <p class="desc">實名制</p>
-                    <div class="note">
-                        <p>
-                            <?php 
-                            try	{
-                                require_once('connectMeetain.php');
-                                if($pdo != false){
-                                        foreach($pdo->query('SELECT COUNT(*) FROM member_realname where mem_realname_situation = "未審核"') as $row) {
-                                        echo $row['COUNT(*)'];
-                                        }
+                                ?>
+                            </p>
+                        </div>
+                    </a></li>
+            </ul>
+            <ul class="verify">
+                <p class="title">審核</p>
+                <li class="page navli realname"><a href="">
+                        <p class="desc">實名制</p>
+                        <div class="note">
+                            <p>
+                                <?php 
+                                try	{
+                                    require_once('connectMeetain.php');
+                                    if($pdo != false){
+                                            foreach($pdo->query('SELECT COUNT(*) FROM member_realname where mem_realname_situation = "未審核"') as $row) {
+                                            echo $row['COUNT(*)'];
+                                            }
 
-                                }	else	{
-                                    echo "<br>失敗ㄌ<br>";
+                                    }	else	{
+                                        echo "<br>失敗ㄌ<br>";
+                                    }
+                                }	catch	(PDOException $e)	{
+                                    echo "錯誤原因：",$e->getMessage(), "<br>";
+                                    echo "錯誤行號：",$e->getLine(),"<br>";
                                 }
-                            }	catch	(PDOException $e)	{
-                                echo "錯誤原因：",$e->getMessage(), "<br>";
-                                echo "錯誤行號：",$e->getLine(),"<br>";
-                            }
-                            ?>
-                        </p>
-                    </div>
-                </a></li>
-            <li class="page navli guide"><a href="">
-                    <p class="desc">嚮導</p>
-                    <div class="note">
-                        <p>
-                            <?php 
-                            try	{
-                                require_once('connectMeetain.php');
-                                if($pdo != false){
-                                        foreach($pdo->query('SELECT COUNT(*) FROM member_guide where mem_guide_situation = "未審核"') as $row) {
-                                        echo $row['COUNT(*)'];
-                                        }
+                                ?>
+                            </p>
+                        </div>
+                    </a></li>
+                <li class="page navli guide"><a href="">
+                        <p class="desc">嚮導</p>
+                        <div class="note">
+                            <p>
+                                <?php 
+                                try	{
+                                    require_once('connectMeetain.php');
+                                    if($pdo != false){
+                                            foreach($pdo->query('SELECT COUNT(*) FROM member_guide where mem_guide_situation = "未審核"') as $row) {
+                                            echo $row['COUNT(*)'];
+                                            }
 
-                                }	else	{
-                                    echo "<br>失敗ㄌ<br>";
+                                    }	else	{
+                                        echo "<br>失敗ㄌ<br>";
+                                    }
+                                }	catch	(PDOException $e)	{
+                                    echo "錯誤原因：",$e->getMessage(), "<br>";
+                                    echo "錯誤行號：",$e->getLine(),"<br>";
                                 }
-                            }	catch	(PDOException $e)	{
-                                echo "錯誤原因：",$e->getMessage(), "<br>";
-                                echo "錯誤行號：",$e->getLine(),"<br>";
-                            }
-                            ?>
-                        </p>
-                    </div>
-                </a></li>
-        </ul>
-        <ul class="page product"><a href="">
-                <p class="title">商品</p>
-            </a></ul>
-        <ul class="page administer"><a href="">
-                <p class="title">管理員</p>
-            </a></ul>
-    </nav>
+                                ?>
+                            </p>
+                        </div>
+                    </a></li>
+            </ul>
+            <ul class="page product"><a href="">
+                    <p class="title">商品</p>
+                </a></ul>
+            <ul class="page administer"><a href="">
+                    <p class="title">管理員</p>
+                </a></ul>
+        </nav>
     </aside>
     <section>
 
--- dev/report.php
+++ dev/report.php
@@ -16,12 +16,10 @@
 </head>
 <body>
 
-<?php
-    require_once("./BackstageHeader.inc");
-?>
 <main>
-
-
+    <?php
+    require_once("./BackstageHeader.inc");
+    ?>
     <aside>
     <nav id="BackstageNav">
         <ul class="page index ulat"><a href="">
@@ -166,7 +164,7 @@
                 <span>未通過</span>
             </div>
             <table>
-                <tr>
+                <!-- <tr>
                     <th>檢舉編號</th>
                     <th>揪團編號</th>
                     <th>揪團標題</th>
@@ -197,7 +195,49 @@
                         <p>送出</p>
                         <div class="bg"></div>
                     </button></td>
-                </tr>
+                </tr> -->
+            <?php 
+                try	{
+                    require_once('connectMeetain.php');
+                    
+                        $sql = 'SELECT tour_report_no "檢舉編號" , tour_report_tour "揪團編號" , tour_title "揪團標題" , tour_hoster "被檢舉人" , tour_report_build "檢舉時間", tour_report_reason "檢舉緣由" from tour_report tr join tour t on tr.tour_report_tour = t.tour_no where tr.tour_report_situation = "未處理";';
+                        $pdoStatement = $pdo->query($sql);
+                        $prodRows = $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
+                        ?>
+                        <table>
+                        <tr class='cyan'><th width="80px">檢舉編號</th><th width="80px">揪團編號</th><th width="300px">揪團標題</th><th width="80px">被檢舉人</th><th width="110px">檢舉時間</th><th width="150px">檢舉緣由</th><th width="230px">檢舉狀態</th></tr>
+                        <?php
+                        foreach ( $prodRows as $i => $prodRow){
+                        ?>
+                            <tr>
+                            <td class='pink'><?=$prodRow["檢舉編號"]?></td>
+                            <td><?=$prodRow["揪團編號"]?></td>
+                            <td style="text-align: left;padding-left: 5px;"><?=$prodRow["揪團標題"]?></td>
+                            <td ><?=$prodRow["被檢舉人"]?></td>
+                            <td><?=$prodRow["檢舉時間"]?></td>
+                            <td style="text-align: left;padding-left: 5px;"><?=$prodRow["檢舉緣由"]?></td>
+                            <td style="text-align: left;padding-left: 10px;">   <label><input type="radio" value="unPass" name="review?<?=$prodRow["檢舉編號"]?>">未通過</label><br>
+                                    <label><input type="radio" value="Pass" name="review?<?=$prodRow["檢舉編號"]?>">通過，禁言</label>
+                                    <select name="BanLong">
+                                        <option value="5">5分鐘</option>
+                                        <option value="3" selected="selected">3天</option>
+                                        <option value="7">7天</option>
+                                        <option value="14">14天</option>
+                                        <option value="28">28天</option>
+                                    </select>
+                            </td>
+                            <td style="background-color: #eaf1f4;" ><button type="submit" class="btnB_L_yellow">
+                                <p>送出</p>
+                                <div class="bg"></div>
+                            </button></td>
+                            </tr>
+
+                            <?php } ?>
+                        </table>
+                    <?php
+                    }	catch	(PDOException $e)	{
+                        }
+                ?>
             </table>
         </div>
     </section>
