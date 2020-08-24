c21d8c619432bb221af0e88f95129d2dcbfae9fb
OwenTheStar <owen0919335293dl4ul@gmail.com>
2020-08-24 09:50:56 +0800

    Report_SQL

--- dev/ShowInFo.php
+++ dev/ShowInFo.php
@@ -60,7 +60,7 @@
 try	{
     require_once('connectMeetain.php');
 	
-		$sql = 'SELECT tour_report_no "檢舉編號" , tour_report_tour "揪團編號" , tour_title "揪團標題" , tour_report_mem "被檢舉人" , tour_report_build "檢舉時間", tour_report_reason "檢舉緣由" from tour_report tr join tour t on tr.tour_report_tour = t.tour_no where tr.tour_report_situation = "未處理";';
+		$sql = 'SELECT tour_report_no "檢舉編號" , tour_report_tour "揪團編號" , tour_title "揪團標題" , tour_hoster "被檢舉人" , tour_report_build "檢舉時間", tour_report_reason "檢舉緣由" from tour_report tr join tour t on tr.tour_report_tour = t.tour_no where tr.tour_report_situation = "未處理";';
 		$pdoStatement = $pdo->query($sql);
 		$prodRows = $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
 		?>
@@ -76,8 +76,8 @@ try	{
              <td><?=$prodRow["被檢舉人"]?></td>
              <td><?=$prodRow["檢舉時間"]?></td>
              <td><?=$prodRow["檢舉緣由"]?></td>
-             <td>   <label><input type="radio" value="unPass" name="review?<?=$prodRow["揪團編號"]?>">未通過</label><br>
-                    <label><input type="radio" value="Pass" name="review?<?=$prodRow["揪團編號"]?>">通過，禁言</label>
+             <td>   <label><input type="radio" value="unPass" name="review?<?=$prodRow["檢舉編號"]?>">未通過</label><br>
+                    <label><input type="radio" value="Pass" name="review?<?=$prodRow["檢舉編號"]?>">通過，禁言</label>
                     <select name="BanLong">
                         <option value="5">5分鐘</option>
                         <option value="3" selected="selected">3天</option>
@@ -99,7 +99,7 @@ try	{
 try	{
     require_once('connectMeetain.php');
 	
-		$sql = 'SELECT tour_report_no "檢舉編號" , tour_report_tour "揪團編號" , tour_title "揪團標題" , tour_report_mem "被檢舉人" , tour_report_build "檢舉時間", tour_report_reason "檢舉緣由", tour_report_banLong "檢舉時長", ban_tour_date "解封時間" from tour_report tr join tour t on tr.tour_report_tour = t.tour_no join member mem on tour_report_mem = mem.mem_no where tr.tour_report_situation = "已處理已通過" ;';
+		$sql = 'SELECT tour_report_no "檢舉編號" , tour_report_tour "揪團編號" , tour_title "揪團標題" , tour_hoster "被檢舉人" , tour_report_build "檢舉時間", tour_report_reason "檢舉緣由", tour_report_banLong "檢舉時長", ban_tour_date "解封時間" from tour_report tr join tour t on tr.tour_report_tour = t.tour_no join member mem on tour_report_mem = mem.mem_no where tr.tour_report_situation = "已處理已通過" ;';
 		$pdoStatement = $pdo->query($sql);
 		$prodRows = $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
 		?>
@@ -129,7 +129,7 @@ try	{
 try	{
     require_once('connectMeetain.php');
 	
-		$sql = 'SELECT tour_report_no "檢舉編號" , tour_report_tour "揪團編號" , tour_title "揪團標題" , tour_report_mem "被檢舉人" , tour_report_build "檢舉時間", tour_report_reason "檢舉緣由",tour_report_situation "檢舉狀態" from tour_report tr join tour t on tr.tour_report_tour = t.tour_no where tr.tour_report_situation = "已處理未通過" ;';
+		$sql = 'SELECT tour_report_no "檢舉編號" , tour_report_tour "揪團編號" , tour_title "揪團標題" , tour_hoster "被檢舉人" , tour_report_build "檢舉時間", tour_report_reason "檢舉緣由",tour_report_situation "檢舉狀態" from tour_report tr join tour t on tr.tour_report_tour = t.tour_no where tr.tour_report_situation = "已處理未通過" ;';
 		$pdoStatement = $pdo->query($sql);
 		$prodRows = $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
 		?>
@@ -176,8 +176,8 @@ try	{
              <td><?=$prodRow["被檢舉人"]?></td>
              <td><?=$prodRow["檢舉時間"]?></td>
              <td><?=$prodRow["檢舉緣由"]?></td>
-             <td>   <label><input type="radio" value="unPass" name="review?<?=$prodRow["揪團編號"]?>">未通過</label><br>
-                    <label><input type="radio" value="Pass" name="review?<?=$prodRow["揪團編號"]?>">通過，禁言</label>
+             <td>   <label><input type="radio" value="unPass" name="review?<?=$prodRow["檢舉編號"]?>">未通過</label><br>
+                    <label><input type="radio" value="Pass" name="review?<?=$prodRow["檢舉編號"]?>">通過，禁言</label>
                     <select name="BanLong">
                         <option value="5">5分鐘</option>
                         <option value="3" selected="selected">3天</option>
@@ -259,24 +259,24 @@ try	{
 try	{
     require_once('connectMeetain.php');
 	
-		$sql = 'SELECT forum_report_no "檢舉編號" , forum_report_post "討論編號" , forum_post_title "討論標題" , forum_post_poster "被檢舉人" , forum_report_build "檢舉時間", forum_report_reason "檢舉緣由" from forum_report fr join forum_post fp on fr.forum_report_post = fp.forum_post_no where fr.forum_report_situation = "未處理" ;';
+		$sql = 'SELECT comment_report_no "檢舉編號" , comment_report_comment "留言編號" , comment_innertext "留言內文" , comment_poster "被檢舉人" , comment_report_build "檢舉時間", comment_report_reason "檢舉緣由" , comment_class "檢舉板塊" from comment_report cr join comment_post cp on cr.comment_report_comment = cp.comment_no where cr.comment_report_situation = "未處理" ;';
 		$pdoStatement = $pdo->query($sql);
 		$prodRows = $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
 		?>
 		<table>
-		<tr class='cyan'><th width="80px">檢舉編號</th><th width="80px">討論編號</th><th width="300px">討論標題</th><th width="80px">被檢舉人</th><th width="110px">檢舉時間</th><th width="150px">檢舉緣由</th><th width="230px">檢舉狀態</th></tr>
+		<tr class='cyan'><th width="80px">檢舉編號</th><th width="80px">留言編號</th><th width="300px">留言內文</th><th width="80px">被檢舉人</th><th width="110px">檢舉時間</th><th width="150px">檢舉緣由</th><th width="230px">檢舉狀態</th></tr>
 		<?php
 		foreach ( $prodRows as $i => $prodRow){
 		?>
 			 <tr>
              <td class='pink'><?=$prodRow["檢舉編號"]?></td>
-             <td><?=$prodRow["討論編號"]?></td>
-             <td><?=$prodRow["討論標題"]?></td>
+             <td><?=$prodRow["留言編號"]?></td>
+             <td><?=$prodRow["留言內文"]?></td>
              <td><?=$prodRow["被檢舉人"]?></td>
              <td><?=$prodRow["檢舉時間"]?></td>
              <td><?=$prodRow["檢舉緣由"]?></td>
-             <td>   <label><input type="radio" value="unPass" name="review?<?=$prodRow["揪團編號"]?>">未通過</label><br>
-                    <label><input type="radio" value="Pass" name="review?<?=$prodRow["揪團編號"]?>">通過，禁言</label>
+             <td>   <label><input type="radio" value="unPass" name="review?<?=$prodRow["檢舉編號"]?>">未通過</label><br>
+                    <label><input type="radio" value="Pass" name="review?<?=$prodRow["檢舉編號"]?>">通過，禁言</label>
                     <select name="BanLong">
                         <option value="5">5分鐘</option>
                         <option value="3" selected="selected">3天</option>
@@ -298,19 +298,19 @@ try	{
 try	{
     require_once('connectMeetain.php');
 	
-		$sql = 'SELECT forum_report_no "檢舉編號" , forum_report_post "討論編號" , forum_post_title "討論標題" , forum_post_poster "被檢舉人" , forum_report_build "檢舉時間", forum_report_reason "檢舉緣由", forum_report_banLong "檢舉時長", ban_forum_date "解封時間" from forum_report fr join forum_post fp on fr.forum_report_post = fp.forum_post_no join member mem on forum_report_mem = mem.mem_no where fr.forum_report_situation = "已處理已通過" ;';
+		$sql = 'SELECT comment_report_no "檢舉編號" , comment_report_comment "留言編號" , comment_innertext "留言內文" , comment_poster "被檢舉人" , comment_report_build "檢舉時間", comment_report_reason "檢舉緣由" , comment_class "檢舉板塊" , comment_report_banLong "檢舉時長", ban_forum_date "討論區解封時間", ban_tour_date "揪團解封時間" from comment_report cr join comment_post cp on cr.comment_report_comment = cp.comment_no join member mem on comment_report_mem = mem.mem_no where cr.comment_report_situation = "已處理已通過" ;';
 		$pdoStatement = $pdo->query($sql);
 		$prodRows = $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
 		?>
 		<table>
-		<tr class='cyan'><th width="80px">檢舉編號</th><th width="80px">討論編號</th><th width="300px">討論標題</th><th width="80px">被檢舉人</th><th width="110px">檢舉時間</th><th width="150px">檢舉緣由</th><th width="230px">檢舉狀態</th></tr>
+		<tr class='cyan'><th width="80px">檢舉編號</th><th width="80px">留言編號</th><th width="300px">留言內文</th><th width="80px">被檢舉人</th><th width="110px">檢舉時間</th><th width="150px">檢舉緣由</th><th width="230px">檢舉狀態</th></tr>
 		<?php
 		foreach ( $prodRows as $i => $prodRow){
 		?>
 			 <tr>
              <td class='pink'><?=$prodRow["檢舉編號"]?></td>
-             <td><?=$prodRow["討論編號"]?></td>
-             <td><?=$prodRow["討論標題"]?></td>
+             <td><?=$prodRow["留言編號"]?></td>
+             <td><?=$prodRow["留言內文"]?></td>
              <td><?=$prodRow["被檢舉人"]?></td>
              <td><?=$prodRow["檢舉時間"]?></td>
              <td><?=$prodRow["檢舉緣由"]?></td>
@@ -328,19 +328,19 @@ try	{
 try	{
     require_once('connectMeetain.php');
 	
-		$sql = 'SELECT forum_report_no "檢舉編號" , forum_report_post "討論編號" , forum_post_title "討論標題" , forum_post_poster "被檢舉人" , forum_report_build "檢舉時間", forum_report_reason "檢舉緣由", forum_report_situation "檢舉狀態" from forum_report fr join forum_post fp on fr.forum_report_post = fp.forum_post_no where fr.forum_report_situation = "已處理未通過" ;';
+		$sql = 'SELECT comment_report_no "檢舉編號" , comment_report_comment "留言編號" , comment_innertext "留言內文" , comment_poster "被檢舉人" , comment_report_build "檢舉時間", comment_report_reason "檢舉緣由" , comment_class "檢舉板塊" , comment_report_situation "檢舉狀態" from comment_report cr join comment_post cp on cr.comment_report_comment = cp.comment_no where cr.comment_report_situation = "已處理未通過" ;';
 		$pdoStatement = $pdo->query($sql);
 		$prodRows = $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
 		?>
 		<table>
-		<tr class='cyan'><th width="80px">檢舉編號</th><th width="80px">討論編號</th><th width="300px">討論標題</th><th width="80px">被檢舉人</th><th width="110px">檢舉時間</th><th width="150px">檢舉緣由</th><th width="230px">檢舉狀態</th></tr>
+		<tr class='cyan'><th width="80px">檢舉編號</th><th width="80px">留言編號</th><th width="300px">留言內文</th><th width="80px">被檢舉人</th><th width="110px">檢舉時間</th><th width="150px">檢舉緣由</th><th width="230px">檢舉狀態</th></tr>
 		<?php
 		foreach ( $prodRows as $i => $prodRow){
 		?>
 			 <tr>
              <td class='pink'><?=$prodRow["檢舉編號"]?></td>
-             <td><?=$prodRow["討論編號"]?></td>
-             <td><?=$prodRow["討論標題"]?></td>
+             <td><?=$prodRow["留言編號"]?></td>
+             <td><?=$prodRow["留言內文"]?></td>
              <td><?=$prodRow["被檢舉人"]?></td>
              <td><?=$prodRow["檢舉時間"]?></td>
              <td><?=$prodRow["檢舉緣由"]?></td>
