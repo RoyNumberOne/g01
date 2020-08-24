e194f680cd2e5418c8c946ca1c16dc8a0bd55226
c926228 <c926228@gmail.com>
2020-08-24 10:57:20 +0800

    follow

--- dev/js/Member.js
+++ dev/js/Member.js
@@ -7,7 +7,43 @@
 //             $(this).addClass('active'). siblings ('.active').removeClass('active');
 //         });
 //     });
+// =========================手機板按鈕======================
 
+var mySwiper = new Swiper(".swiper-container", {
+  pagination: ".pagination",
+  paginationClickable: true,
+  slidesPerView: 4,
+  loop: false
+});
+mySwiper.on("slideChangeStart", function() {
+  console.log("slide change start");
+  $(".swiper-slide-next").addClass("swiper-slide-active");
+});
+/* Trick to fix clickable links in Slides */
+$(".swiper-container .swiper-slide a").bind("click", function() {
+  window.open($(this).attr("href"), "_blank");
+});
+
+// =======================手機板按鈕========================
+//========================================================
+function tabId(evt, className) {
+  var i, x, tabpage;
+  x = document.getElementsByClassName("pages");
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  tabpage = document.getElementsByClassName("tabbutton");
+  for (i = 0; i < x.length; i++) {
+     tabpage[i].classList.remove("highlight");
+  }
+  document.getElementById(className).style.display = "block";
+  evt.currentTarget.classList.add("highlight");
+}
+
+var mybtn = document.getElementsByClassName("now")[0];
+mybtn.click();
+
+//========================================================
 // =========================收藏頁頁簽======================
 function openClass(evt, className) {
     var i, x, tablinks;
@@ -22,31 +58,28 @@ function openClass(evt, className) {
     document.getElementById(className).style.display = "block";
     evt.currentTarget.classList.add("red");
   }
-// =========================收藏頁頁簽======================
-// =========================手機板按鈕======================
-
-var mySwiper = new Swiper(".swiper-container", {
-   pagination: ".pagination",
-   paginationClickable: true,
-   slidesPerView: 4,
-   loop: true
- });
- mySwiper.on("slideChangeStart", function() {
-   console.log("slide change start");
-   $(".swiper-slide-next").addClass("swiper-slide-active");
- });
- /* Trick to fix clickable links in Slides */
- $(".swiper-container .swiper-slide a").bind("click", function() {
-   window.open($(this).attr("href"), "_blank");
- });
-
-
-
-// =======================手機板按鈕========================
-// =======================收藏頁頁簽========================
 
 var mybtn = document.getElementsByClassName("btn")[0];
 mybtn.click();
-
-// 我也不知道為甚麼放在這裡才不會衝突
 // =======================收藏頁頁簽========================
+// =========================揪團頁頁簽======================
+// function travelchange(evt, className) {
+//   var i, x, tabtravels;
+//   x = document.getElementsByClassName("travel_pages");
+//   for (i = 0; i < x.length; i++) {
+//      x[i].style.display = "none";
+//   }
+//   tabtravels = document.getElementsByClassName("tabtravel");
+//   for (i = 0; i < x.length; i++) {
+//      tablinks[i].classList.remove("red");
+//   }
+//   document.getElementById(className).style.display = "block";
+//   evt.currentTarget.classList.add("red");
+// }
+
+// var mybtn = document.getElementsByClassName("this")[0];
+// mybtn.click();
+// =======================揪團頁頁簽========================
+
+
+
