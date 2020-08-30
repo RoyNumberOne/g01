105c7106bb874d550e6b6872d8fc21cd6374eb8b
Hiko <toshihikomit@gmail.com>
2020-08-31 00:58:29 +0800

    新增及調整js排序

--- dev/js/Member_v2.js
+++ dev/js/Member_v2.js
@@ -1,53 +1,4 @@
-// =========================手機板按鈕======================
-
-var mySwiper = new Swiper(".swiper-container", {
-  pagination: ".pagination",
-  paginationClickable: true,
-  slidesPerView: 4,
-  loop: false
-});
-// mySwiper.on("slideChangeStart", function() {
-//   console.log("slide change start");
-//   $(".swiper-slide-next").addClass("swiper-slide-active");
-// });
-
-// /* Trick to fix clickable links in Slides */
-
-// $(".swiper-container .swiper-slide a").bind("click", function() {
-//   window.open($(this).attr("href"), "_blank");
-// });
-
-// =======================手機板切換======================
-$(document).ready(function(){
-  $('.nav_mob').click(function(){
-      $('.nav_list').toggle();
-  });
-  $('.X_btn').click(function(){
-      $('.nav_list').css('display','none');
-  });
-});
-
-//========================桌機板按鈕======================
-
-// function tabId(evt, className) {
-//   var i, x, tabpage;
-//   x = document.getElementsByClassName("pages");
-//   for (i = 0; i < x.length; i++) {
-//      x[i].style.display = "none";
-//   }
-//   tabpage = document.getElementsByClassName("tabbutton");
-//   for (i = 0; i < x.length; i++) {
-//      tabpage[i].classList.remove("highlight");
-//   }
-//   document.getElementById(className).style.display = "block";
-//   evt.currentTarget.classList.add("highlight");
-// }
-
-// var mybtn = document.getElementsByClassName("now")[0];
-// mybtn.click();
-
-//========================桌機板按鈕====================
-// =======================個資頭像修改======================
+// =======================001個資頭像修改======================
 function readURL(input){
   if(input.files && input.files[0]){
       var reader = new FileReader();
@@ -63,7 +14,27 @@ $("#imgInp").change(function(){
 readURL(this);
 });
 // =======================個資頭像修改======================
-// =========================收藏頁頁簽======================
+//========================002桌機板按鈕========================
+
+function tabId(evt, className) {
+  var i, x, tabpage;
+  x = document.getElementsByClassName("pages");
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  tabpage = document.getElementsByClassName("tabbutton");
+  for (i = 0; i < x.length; i++) {
+     tabpage[i].classList.remove("highlight");
+  }
+  document.getElementById(className).style.display = "block";
+  evt.currentTarget.classList.add("highlight");
+}
+
+var mybtn = document.getElementsByClassName("now")[0];
+mybtn.click();
+
+// ========================桌機板按鈕=======================
+// =========================003收藏頁頁簽======================
 function openClass(evt, className) {
   var i, x, tablinks;
   x = document.getElementsByClassName("class");
@@ -81,7 +52,7 @@ function openClass(evt, className) {
 var mybtn = document.getElementsByClassName("btn")[0];
 mybtn.click();
 // =======================收藏頁頁簽========================
-// =====================足跡頁地區切換======================
+// =====================004足跡頁地區切換======================
 function tabArea(evt, className) {
   var i, x, tabareas;
   x = document.getElementsByClassName("area");
