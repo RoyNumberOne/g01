4f57ff0942b98145fad930ee70275dc5c42ed03b
hsuanman <hsuanmanp@gmail.com>
2020-09-01 03:19:30 +0800

    backstagePageV1product

--- dev/backstage/Backstageproduct.php
+++ dev/backstage/Backstageproduct.php
@@ -163,12 +163,28 @@
                     <button type="submit" class="btnB_L_yellow_2" id="loadButton_3"><p>新增</p><div class="bg2"></div></button>
             </div>
             <div id="ccc">
+                <h3>商品 - 上架中</h3>
                 <?php 
                     try	{
-                        require_once('connectMeetain.php');                    
-                            $sql = 'SELECT product_no "商品編號" , degree_category "難度等級" , product_category "商品分類" , product_name "商品名稱" , product_price "商品價格" , product_description "商品說明" , product_image1 "商品圖片一" , product_image2 "商品圖片二" , product_image3 "商品圖片三" from product where product_situation = 1; ';
-                            $pdoStatement = $pdo->query($sql);
-                            $prodRows = $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
+                        require_once('connectMeetain.php');
+
+                            //算出有幾「 筆 」（取得總比數）
+                            $sql = "select count(*) totalCount from product where product_situation=1";
+                            $stmt = $pdo->query($sql); 
+                            $row = $stmt->fetch(PDO::FETCH_ASSOC); 
+                            $totalRecords = $row["totalCount"]; 
+                            // $totalRecords---> 20筆
+                            //每頁要印幾筆
+                            $recPerPage= 5;
+                            
+                            //算出有幾頁 總筆數/我每頁要印的東西
+                            $totalPages = ceil($totalRecords / $recPerPage);
+                            $pageNo = isset($_GET["pageNo"]) ? $_GET["pageNo"] : 1;
+                            //取回資料
+                            $start = ($pageNo-1) * $recPerPage; //要顯示的比數 ===>> 頁數減一 X 每頁要印幾筆
+                            $sql = "SELECT product_no '商品編號' , degree_category '難度等級' , product_category '商品分類' , product_name '商品名稱' , product_price '商品價格' , product_description '商品說明' , product_image1 '商品圖片一' , product_image2 '商品圖片二' , product_image3 '商品圖片三' from product where product_situation=1 limit $start,$recPerPage";
+                            $products = $pdo->query($sql);
+                            $prodRows = $products->fetchAll(PDO::FETCH_ASSOC);
                             ?>
                             <table>
                             <tr class='cyan'><th width="80px">商品編號</th><th width="100px">難度等級</th><th width="100px">商品分類</th><th width="160px">商品名稱</th><th width="80px">商品價格</th><th width="300px">商品說明</th><th width="100px">商品圖片一</th><th width="100px">商品圖片二</th><th width="100px">商品圖片三</th></tr>
@@ -179,16 +195,10 @@
                                 <td class='pink'><?=$prodRow["商品編號"]?></td>
                                 <td><?=$prodRow["難度等級"]?></td>
                                 <td><?=$prodRow["商品分類"]?></td>
-                                <td><?=$prodRow["商品名稱"]?></td>
+                                <td style="text-align: left;"><?=mb_substr($prodRow["商品名稱"],0,10)?></td>
                                 <td><?=$prodRow["商品價格"]?></td>
-                                <td style="overflow:hidden;
-                                            white-space: nowrap;
-                                            text-overflow: ellipsis;
-                                            display: -webkit-box;
-                                            -webkit-line-clamp: 2;
-                                            -webkit-box-orient: vertical;
-                                            white-space: normal;">
-                                            <?=$prodRow["商品說明"]?></td>
+                                <td style="text-align: left;">
+                                            <?=mb_substr($prodRow["商品說明"],0,25)?></td>
                                 <td><?=$prodRow["商品圖片一"]?></td>
                                 <td><?=$prodRow["商品圖片二"]?></td>
                                 <td><?=$prodRow["商品圖片三"]?></td>
@@ -204,7 +214,7 @@
                         }	catch	(PDOException $e)	{
                         }
                     ?>
-                <div class="pagebtn">
+            <!-- <div class="pagebtn">
                 <button class="btnA_S pgprev"><p style="transform: translate(-5px , 0px); transition: 0s;">&#10229</p></button>
                 <button class="btnA_S pg -active"><p>01</p></button>
                 <button class="btnA_S pg"><p>02</p></button>
@@ -212,6 +222,13 @@
                 <button class="btnA_S pg"><p>04</p></button>
                 <button class="btnA_S pg"><p>05</p></button>
                 <button class="btnA_S pgnext"><p style="transform: translate(-5px , 0px); transition: 0s;">&#10230</p></button>
+            </div> -->
+            <div class="pagebtn">
+                <?php 
+                    for($i=1; $i<=$totalPages; $i++){
+                    echo "<a href='BackstageProduct.php?pageNo=$i'><button class=\"btnA_S pg\"`><p>$i</p></button></a>&nbsp;&nbsp;";
+                    }
+                ?>
             </div>
         </div>
     </section>
