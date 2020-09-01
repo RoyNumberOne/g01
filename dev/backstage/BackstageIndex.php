ed35f90d5e4584705811464f5ea4dc474ebb55ae
hsuanman <hsuanmanp@gmail.com>
2020-09-01 18:41:16 +0800

    0901後台變更V1

--- dev/backstage/BackstageIndex.php
+++ dev/backstage/BackstageIndex.php
@@ -9,6 +9,7 @@
     <link rel=“stylesheet” href="./css/fa5.14.0v4.min.css">
     <link rel="stylesheet" href="../css/Backstagemain.css">
     <link rel="stylesheet" href="../css/BackstageHeader.css">
+    <link rel="stylesheet" href="../css/BackstageIndex.css">
     <script src="./js/jquery-3.5.1.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
@@ -20,138 +21,9 @@
         require_once("./BackstageHeader.inc");
     ?>
     <aside>
-        <nav id="BackstageNav">
-            <ul class="page index ulat"><a href="./BackstageIndex.php">
-                    <p class="title" style="color:#1A95DB;">首頁</p>
-                </a></ul>
-            <ul class="report">
-                <p class="title">檢舉</p>
-                <li class="page navli tour"><a href="./BackstageTourReport.php">
-                        <p class="desc">揪團</p>
-                        <div class="note">
-                            <p>
-                                <?php 
-                                try	{
-                                    require_once('connectMeetain.php');
-                                    if($pdo != false){
-                                            foreach($pdo->query('SELECT COUNT(*) FROM tour_report tr join tour t on tr.tour_report_tour = t.tour_no where tr.tour_report_situation = "未處理"') as $row) {
-                                            echo $row['COUNT(*)'];
-                                            }
-
-                                    }	else	{
-                                        echo "<br>失敗ㄌ<br>";
-                                    }
-                                }	catch	(PDOException $e)	{
-                                    echo "錯誤原因：",$e->getMessage(), "<br>";
-                                    echo "錯誤行號：",$e->getLine(),"<br>";
-                                }
-                                ?>
-                            </p>
-                        </div>
-                    </a></li>
-                <li class="page navli forum"><a href="./BackstageForumReport.php">
-                        <p class="desc">討論</p>
-                        <div class="note">
-                            <p>
-                                <?php 
-                                try	{
-                                    require_once('connectMeetain.php');
-                                    if($pdo != false){
-                                        foreach($pdo->query('SELECT COUNT(*) FROM forum_report fr join forum_post fp on fr.forum_report_post = fp.forum_post_no where fr.forum_report_situation = "未處理"') as $row) {
-                                            echo $row['COUNT(*)'];
-                                            }
-
-                                    }	else	{
-                                        echo "<br>失敗ㄌ<br>";
-                                    }
-                                }	catch	(PDOException $e)	{
-                                    echo "錯誤原因：",$e->getMessage(), "<br>";
-                                    echo "錯誤行號：",$e->getLine(),"<br>";
-                                }
-                                ?>
-                            </p>
-                        </div>
-                    </a></li>
-                <li class="page navli comment"><a href="./BackstageCommentReport.php">
-                        <p class="desc">留言</p>
-                        <div class="note">
-                            <p>
-                                <?php 
-                                try	{
-                                    require_once('connectMeetain.php');
-                                    if($pdo != false){
-                                            foreach($pdo->query('SELECT COUNT(*) FROM comment_report cr JOIN comment_post cp ON cr.comment_report_comment = cp.comment_no WHERE cr.comment_report_situation = "未處理"') as $row) {
-                                            echo $row['COUNT(*)'];
-                                            }
-
-                                    }	else	{
-                                        echo "<br>失敗ㄌ<br>";
-                                    }
-                                }	catch	(PDOException $e)	{
-                                    echo "錯誤原因：",$e->getMessage(), "<br>";
-                                    echo "錯誤行號：",$e->getLine(),"<br>";
-                                }
-                                ?>
-                            </p>
-                        </div>
-                    </a></li>
-            </ul>
-            <ul class="verify">
-                <p class="title">審核</p>
-                <li class="page navli realname"><a href="./BackstageMemRealname.php">
-                        <p class="desc">實名制</p>
-                        <div class="note">
-                            <p>
-                                <?php 
-                                try	{
-                                    require_once('connectMeetain.php');
-                                    if($pdo != false){
-                                            foreach($pdo->query('SELECT COUNT(*) FROM member_realname where mem_realname_situation = "未審核"') as $row) {
-                                            echo $row['COUNT(*)'];
-                                            }
-
-                                    }	else	{
-                                        echo "<br>失敗ㄌ<br>";
-                                    }
-                                }	catch	(PDOException $e)	{
-                                    echo "錯誤原因：",$e->getMessage(), "<br>";
-                                    echo "錯誤行號：",$e->getLine(),"<br>";
-                                }
-                                ?>
-                            </p>
-                        </div>
-                    </a></li>
-                <li class="page navli guide"><a href="./BackstageMemGuide.php">
-                        <p class="desc">嚮導</p>
-                        <div class="note">
-                            <p>
-                                <?php 
-                                try	{
-                                    require_once('connectMeetain.php');
-                                    if($pdo != false){
-                                            foreach($pdo->query('SELECT COUNT(*) FROM member_guide where mem_guide_situation = "未審核"') as $row) {
-                                            echo $row['COUNT(*)'];
-                                            }
-
-                                    }	else	{
-                                        echo "<br>失敗ㄌ<br>";
-                                    }
-                                }	catch	(PDOException $e)	{
-                                    echo "錯誤原因：",$e->getMessage(), "<br>";
-                                    echo "錯誤行號：",$e->getLine(),"<br>";
-                                }
-                                ?>
-                            </p>
-                        </div>
-                    </a></li>
-            </ul>
-            <ul class="page product"><a href="./BackstageProduct.php">
-                    <p class="title">商品</p>
-                </a></ul>
-            <ul class="page administer"><a href="./BackstageAdministrator.php">
-                    <p class="title">管理員</p>
-                </a></ul>
-        </nav>
+    <?php
+        require_once("./BackstageNav.inc");
+    ?>        
     </aside>
     <section>
 
