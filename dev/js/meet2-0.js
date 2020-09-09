b09483ec86df95bb218abe03ed261d70fa77d449
c926228 <c926228@gmail.com>
2020-09-09 11:40:40 +0800

    json>php整理js

--- dev/js/meet2-0.js
+++ dev/js/meet2-0.js
@@ -61,5 +61,80 @@ $(function() {
     });
 });
         
+//<!-- 換icon card/list-->
+$(document).ready(function(){
+    $(".icon_card").click(function(){
+        if($(".icon_card").attr('src') === "./images/icons/icon_card_w.svg"){
+            $(this).attr("src","./images/icons/icon_heart_h&c.svg");
+        }else{
+            $(this).attr("src","./images/icons/icon_card_w.svg");
+        }
+    });
+});
 
+//<!-- 收藏 換愛心 -->
+$(document).ready(function(){
+    $(".heart").click(function(){
+        if($(this).attr('src') === "./images/icons/icon_heart.svg"){
+            $(this).attr("src","./images/icons/icon_heart_h&c.svg");
+        }else{
+            $(this).attr("src","./images/icons/icon_heart.svg");
+        }
+    });
+});
 
+//<!----go top裡面的箭頭需要用到的js----->
+$(function() {
+    /* 按下GoTop按鈕時的事件 */
+    $('#gotop').click(function(){
+        $('html,body').animate({ scrollTop: 0 }, 'slow');   /* 返回到最頂上 */
+        return false;
+    });
+    
+    /* 偵測卷軸滑動時，往下滑超過400px就讓GoTop按鈕出現 */
+    $(window).scroll(function() {
+        if ( $(this).scrollTop() > 400){
+            $('#gotop').fadeIn();
+        } else {
+            $('#gotop').fadeOut();
+        }
+    });
+});
+
+//<!-- 頁碼 -->
+function checkpg(){
+    if ($(".pgprev").next().hasClass("-active")) {
+        $(".pgprev").css("visibility","hidden");
+    }   else {
+        $(".pgprev").css("visibility","visible");
+    }
+    if ($(".pgnext").prev().hasClass("-active")) {
+        $(".pgnext").css("visibility","hidden");
+    }   else{
+        $(".pgnext").css("visibility","visible");
+    }
+}
+
+checkpg();
+
+$(".pg").click(function(){
+    $(this).parent().children().removeClass("-active");
+    $(this).addClass("-active");
+    checkpg();
+});
+
+$(".pgprev").click(function(){
+    if (!$(".pgprev").next().hasClass("-active")) {
+        $(".-active").prev().addClass("-active");
+        $(".-active").next(".-active").removeClass("-active");
+    }
+    checkpg();
+});
+
+$(".pgnext").click(function(){
+    if (!$(".pgnext").prev().hasClass("-active")) {
+        $(".-active").next().addClass("-active");
+        $(".-active").prev(".-active").removeClass("-active");
+    }
+    checkpg();
+});
