39c38cf73deabc95d700ec42222c24ad503d8e41
Hiko <toshihikomit@gmail.com>
2020-09-14 03:15:48 +0800

    會員文章OK(暫缺最新回覆sql)

--- dev/INDIVIDUAL/hiko/member_v2.sql
+++ dev/INDIVIDUAL/hiko/member_v2.sql
@@ -17,5 +17,22 @@ select tk.tour_keep_mem'誰收藏', tk.tour_iskept_tour'收藏哪團', T.tour_no
 select pk.product_keep_mem'誰收藏', pk.product_iskept_no'收藏哪項', P.product_no'商品編號', P.product_image1'商品圖1', P.product_name'商品名稱', P.product_description'商品說明'
 	from product_keep pk
 		join product P on(pk.product_iskept_no = P.product_no)
-        WHERE pk.product_keep_mem = 10008  -- 之後改變數and
-        order BY P.product_no DESC; -- 因為沒有建立日期所以用編號排序
+    WHERE pk.product_keep_mem = 10008  -- 之後改變數and
+    order BY P.product_no DESC; -- 因為沒有建立日期所以用編號排序
+
+----------------------------------------------------------------------------------------------------------
+
+-- 會員發起的揪團，由出團時間近到遠排序
+select tp.tour_participate_mem'誰收藏', tp.tour_participate_tour'收藏哪團', T.tour_no'揪團編號', T.tour_image_1'圖片上傳1', T.tour_title'揪團文標題',T.tour_apply'已報名人數', T.tour_activitystart'活動開始日期'
+	from tour_participate tp  -- 還少了山的難度跟山的地區
+		join tour T on(tp.tour_participate_tour = T.tour_no)
+	WHERE tp.tour_participate_mem = 10008  -- 之後改變數
+    order BY T.tour_activitystart DESC;
+
+----------------------------------------------------------------------------------------------------------
+
+-- 會員發佈的文章，由發布時間新到舊排序
+select forum_post_poster'討論文發文者', forum_post_no'討論文編號', forum_post_category'討論文類別', forum_post_image'討論文圖片', forum_post_time'發文時間', forum_post_title, forum_post_innertext'討論文內文', (forum_post_situation = 1)'討論文狀態'
+	from forum_post
+    where forum_post_poster = 10008 -- 之後改變數
+	order BY forum_post_time DESC;
