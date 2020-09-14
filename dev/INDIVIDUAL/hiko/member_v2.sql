a958cb0fa8f535d5df2d59c2f11b9a5edaaedf89
Hiko <toshihikomit@gmail.com>
2020-09-14 21:31:51 +0800

    足跡頁40%

--- dev/INDIVIDUAL/hiko/member_v2.sql
+++ dev/INDIVIDUAL/hiko/member_v2.sql
@@ -1,3 +1,13 @@
+
+-- 會員已完成的揪團，由出團時間近到遠排序
+select tp.tour_participate_mem'誰參加', tp.tour_participate_tour'參加哪團', T.tour_no'揪團編號',T.tour_progress'揪團狀態', T.tour_activitystart'活動開始日期', T.tour_activityend'活動結束日期', M.mountain_image'山的圖片', M.mountain_area'山的地區', M.mountain_name'山的名字'
+	from tour_participate tp
+		join tour T on(tp.tour_participate_tour = T.tour_no)
+        join mountain M on(T.tour_mountain = M.mountain_no)
+	WHERE tp.tour_participate_mem = 10009  -- and T.tour_progress = '已結束'
+	order BY T.tour_activitystart DESC;
+
+----------------------------------------------------------------------------------------------------------
 -- 會員收藏討論文章，由新到舊排序
 select fk.forum_keep_mem'誰收藏', fk.forum_iskept_post'收藏哪篇' ,fp.forum_post_no'討論文章編號' ,fp.forum_post_category'討論文章類別' ,fp.forum_post_title'討論文標題' ,fp.forum_post_image'討論文圖片' ,fp.forum_post_innertext'討論文內文' ,fp.forum_post_time'發文時間' 
 	from forum_keep fk 
@@ -37,6 +47,7 @@ select tp.tour_participate_mem'誰參加', tp.tour_participate_tour'參加哪團
         join mountain M on(T.tour_mountain = M.mountain_no)
 	WHERE tp.tour_participate_mem = 10009  -- 之後改變數
 	order BY T.tour_activitystart DESC;
+
 ----------------------------------------------------------------------------------------------------------
 
 -- 會員發佈的文章，由發布時間新到舊排序
