19cd93d4ccaca35421d44c47f0b377967e94d5b4
hsuanman <hsuanmanp@gmail.com>
2020-09-15 01:55:55 +0800

    購物車訂單0914v2

--- dev/shoppingcartorder.php
+++ dev/shoppingcartorder.php
@@ -3,6 +3,7 @@ try {
     require_once ('connectMeetain.php');
     $member_no = 10001;
     $order_logistics_deliver = $_POST['order_logistics_deliver'];
+    $order_logistics_fee = $_POST['order_logistics_fee'];
     $order_cashflow = $_POST['order_cashflow'];
     $order_total = $_POST['order_total'];
     // $order_total = 24000;
@@ -10,8 +11,8 @@ try {
     $order_logistics_phone = $_POST['order_logistics_phone'];
     $order_logistics_address = $_POST['order_logistics_address'];
     //新增
-	$sql = "INSERT INTO orders (member_no,order_logistics_deliver, order_cashflow , order_total , order_logistics_recipient , order_logistics_phone , order_logistics_address )
-    VALUES ('$member_no','$order_logistics_deliver', '$order_cashflow','$order_total','$order_logistics_recipient','$order_logistics_phone','$order_logistics_address');";
+	$sql = "INSERT INTO orders (member_no,order_logistics_deliver, order_logistics_fee , order_cashflow , order_total , order_logistics_recipient , order_logistics_phone , order_logistics_address )
+    VALUES ('$member_no','$order_logistics_deliver','$order_logistics_fee','$order_cashflow','$order_total','$order_logistics_recipient','$order_logistics_phone','$order_logistics_address');";
     $pdoStatement = $pdo->query($sql);
     
     if($order_cashflow =='點數付款' ){
