8608929f8729e63fdf45948fd72490b70e27fcbe
hsuanman <hsuanmanp@gmail.com>
2020-09-16 00:15:46 +0800

    修正一下

--- dev/shoppingcartPoints.php
+++ dev/shoppingcartPoints.php
@@ -7,7 +7,7 @@
 	try	{
         require_once('connectMeetain.php');
         // $mem_no = ; 先給預設 這裡之後要抓session的會員身份
-        $mem_no = 10001;
+        $member_no = 10001;
         $sql = "SELECT mem_point FROM member WHERE `mem_no`= $member_no";
         $pdoStatement = $pdo->query($sql);
 		$prodRows = $pdoStatement->fetch(PDO::FETCH_ASSOC);
