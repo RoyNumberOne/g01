f296cf7fc59417b071cba9a57818aecc1b76798e
hsuanman <hsuanmanp@gmail.com>
2020-09-17 16:39:35 +0800

    購物車會員登入訂單

--- dev/shoppingcartorder.php
+++ dev/shoppingcartorder.php
@@ -1,33 +1,29 @@
 <?php
-// session_start();
-// if (isset($_SESSION["mem_acc"]) === true){
-//     //送出登入者的姓名資料
-//     $member_no = $_SESSION["mem_no"];
-// }else    {
-//     echo "{}";
-// }
-try {
-    require_once ('connectMeetain.php');
-    $member_no = 10001;
+session_start();
+if (isset($_SESSION["mem_acc"]) === true){
+    //送出登入者的姓名資料
+    $member_no = $_SESSION["mem_no"];
     $order_logistics_deliver = $_POST['order_logistics_deliver'];
     $order_logistics_fee = $_POST['order_logistics_fee'];
     $order_cashflow = $_POST['order_cashflow'];
     $order_total = $_POST['order_total'];
-    // $order_total = 24000;
     $order_logistics_recipient = $_POST['order_logistics_recipient'];
     $order_logistics_phone = $_POST['order_logistics_phone'];
     $order_logistics_address = $_POST['order_logistics_address'];
-    //新增
-	$sql = "INSERT INTO orders (member_no,order_logistics_deliver, order_logistics_fee , order_cashflow , order_total , order_logistics_recipient , order_logistics_phone , order_logistics_address )
-    VALUES ('$member_no','$order_logistics_deliver','$order_logistics_fee','$order_cashflow','$order_total','$order_logistics_recipient','$order_logistics_phone','$order_logistics_address');";
-    $pdoStatement = $pdo->query($sql);
-        foreach ($_REQUEST["arr"] as $key => $value) {
-                $product_number = $value['numbers'];
-                $product_no = $value['product'];
-                $product_price = $value['price'];
-        $sql = "INSERT INTO order_list (order_no,product_no, product_number, product_price) VALUES (last_insert_id(),'$product_no','$product_number','$product_price');";
-                $pdoStatement = $pdo->query($sql);
-}
+    $arr = json_decode($_POST['arr'], true);
+    try {
+        require_once ('connectMeetain.php');
+        //新增
+        $sql = "INSERT INTO orders (member_no,order_logistics_deliver, order_logistics_fee , order_cashflow , order_total , order_logistics_recipient , order_logistics_phone , order_logistics_address )
+        VALUES ('$member_no','$order_logistics_deliver','$order_logistics_fee','$order_cashflow','$order_total','$order_logistics_recipient','$order_logistics_phone','$order_logistics_address');";
+        $pdoStatement = $pdo->query($sql);
+            foreach($arr as $key => $value) {
+                    $product_number = $value['numbers'];
+                    $product_no = $value['product'];
+                    $product_price = $value['price'];
+            $sql = "INSERT INTO order_list (order_no,product_no, product_number, product_price) VALUES (last_insert_id(),'$product_no','$product_number','$product_price');";
+                    $pdoStatement = $pdo->query($sql);
+            }
    
     if($order_cashflow =='點數付款' ){
         //查詢
@@ -44,12 +40,16 @@ try {
         $memberpoint->bindValue(":mem_point",$my_points);
         $memberpoint->execute();
     }
-    // echo "New record created successfully";
+    echo "訂單已送出";
     }
-catch(PDOException $e)
-    {
-    	// echo $sql . "<br>" . $e->getMessage();
-    }
+    
+    catch(PDOException $e)
+        {   
+            echo $sql . "<br>" . $e->getMessage();
+        }
   
-?>
+}else    {
+    echo "請登入會員後購買";
+}
 
+?>
