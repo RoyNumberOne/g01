4ebec47da44b0fa9fd51f4c20a7ae12668a45c70
WanTing-Zou <wting2412@gmail.com>
2020-09-17 17:21:29 +0800

    我要發文OK

--- dev/phpForConnect/forumPostNormal.php
+++ dev/phpForConnect/forumPostNormal.php
@@ -10,13 +10,13 @@ try{
             LEFT OUTER JOIN member_realname r ON ( f.forum_post_poster = r.mem_no and r.mem_realname_situation = '已審核已通過')
             LEFT OUTER JOIN member_guide g ON ( f.forum_post_poster = g.mem_no and g.mem_guide_situation = '已審核已通過')
             JOIN member m ON f.forum_post_poster = m.mem_no
-            JOIN comment_post c ON (f.forum_post_no = c.forum_post_no  and c.comment_situation = 1)
+            LEFT OUTER JOIN comment_post c ON (f.forum_post_no = c.forum_post_no  and c.comment_situation = 1)
             LEFT OUTER JOIN forum_keep fk on fk .forum_iskept_post = f.forum_post_no
             LEFT OUTER JOIN achievement a1 on m.mem_badge1 = a1.achievement_no
             LEFT OUTER JOIN achievement a2 on m.mem_badge2 = a2.achievement_no
             LEFT OUTER JOIN achievement a3 on m.mem_badge3 = a3.achievement_no
     WHERE f.forum_post_situation = 1 and forum_post_category not in ('公告')
-    GROUP BY f.forum_post_poster,c.forum_post_no,r.mem_realname , g.guide_no 
+    GROUP BY f.forum_post_no,f.forum_post_poster,c.forum_post_no,r.mem_realname , g.guide_no 
     ORDER BY f.forum_post_time DESC , f.forum_post_no DESC";
 
     $pdoStatement = $pdo->query($sql);
