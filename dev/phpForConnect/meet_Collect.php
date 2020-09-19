506bd1b31a018471ca9381488de1540478352811
hsuanman <hsuanmanp@gmail.com>
2020-09-20 00:33:36 +0800

    揪團收藏修改OK

--- dev/phpForConnect/meet_Collect.php
+++ dev/phpForConnect/meet_Collect.php
@@ -2,22 +2,22 @@
 session_start();
 if (isset($_SESSION["mem_acc"]) === true){
     //送出登入者的姓名資料
-    $tour_participate_tour = $_POST["tour_participate_tour"];
+    $tour_no = $_POST["tour_no"];
     $member_no = $_SESSION["mem_no"];
     try {
         require_once ('connectMeetain.php');
         //查詢
-        $sql = "SELECT tour_keep_mem , tour_iskept_tour from tour_keep where tour_keep_mem = $member_no and tour_iskept_tour = $tour_participate_tour";
+        $sql = "SELECT tour_keep_mem , tour_iskept_tour from tour_keep where tour_keep_mem = $member_no and tour_iskept_tour = $tour_no";
         $pdoStatement = $pdo->query($sql);
         $prodRowCounts = $pdoStatement->rowCount();
         // echo $prodRowCounts;
         //新增
         if($prodRowCounts == 0){
-            $sql_1 = "INSERT INTO `tour_keep` (`tour_keep_mem`,`tour_iskept_tour`) VALUES ($member_no, $tour_participate_tour);";
+            $sql_1 = "INSERT INTO `tour_keep` (`tour_keep_mem`,`tour_iskept_tour`) VALUES ($member_no, $tour_no);";
             $pdoStatement = $pdo->query($sql_1);
             // echo "已加入收藏";
         }else{
-            $sql_2="DELETE FROM `tour_keep` WHERE `tour_keep_mem` = $member_no AND `tour_iskept_tour` = $tour_participate_tour;";
+            $sql_2="DELETE FROM `tour_keep` WHERE `tour_keep_mem` = $member_no AND `tour_iskept_tour` = $tour_no;";
             $pdoStatement = $pdo->query($sql_2);
             // echo "已移除收藏";
         }
--- dev/phpForConnect/meet_Collect_pic.php
+++ dev/phpForConnect/meet_Collect_pic.php
@@ -2,15 +2,15 @@
 session_start();
 if (isset($_SESSION["mem_acc"]) === true){
     //送出登入者的姓名資料
-    $tour_participate_tour = $_POST["tour_participate_tour"];
+    $tour_no = $_POST["tour_no"];
     $member_no = $_SESSION["mem_no"];
     try {
         require_once ('connectMeetain.php');
         //查詢
-        $sql = "SELECT tour_keep_mem , tour_iskept_tour from tour_keep where tour_keep_mem = $member_no and tour_iskept_tour = $tour_participate_tour";
+        $sql = "SELECT tour_keep_mem , tour_iskept_tour from tour_keep where tour_keep_mem = $member_no and tour_iskept_tour = $tour_no";
         $pdoStatement = $pdo->query($sql);
         $prodRowCounts = $pdoStatement->rowCount();
-        // echo $prodRowCounts;    
+        echo $prodRowCounts;    
     }
     
     catch(PDOException $e)
