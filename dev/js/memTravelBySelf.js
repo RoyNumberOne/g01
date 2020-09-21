377553ce005b817464a298434110eb62351b905a
Hiko <toshihikomit@gmail.com>
2020-09-21 15:18:53 +0800

    難度顯示修正

--- dev/js/memTravelBySelf.js
+++ dev/js/memTravelBySelf.js
@@ -5,27 +5,6 @@ new Vue({
         TravelBySelfList: [],  //BBB
         TravelBySelfIndex: [],  //CCC
     },
-
-    mounted(){
-        axios.get('./phpForConnect/memTravelBySelf.php') //根據哪個php
-        // axios.get(`./phpForConnect/memTravelBySelf.php?test=${test}`)
-
-        .then((res) => {
-            this.TravelBySelfList = res.data; //this.BBB = res.data;
-
-            console.log(res.data); //測試是否成功
-
-            // console.log(this.meetList);
-            for(let i = 0; i< this.TravelBySelfList.length; i++){  //動態生成內容，依據json有幾筆
-                this.TravelBySelfIndex.push(i)
-            }
-            // for(let i = 0; i< this.BBB.length; i++){  
-            //     this.CCC.push(i)
-            // }
-
-        })
-        .catch(error => {console.log(error)}); 
-    },
     filters: {
         // var mountain_area = this.Tour1.mountain_area;
         Area: function(value) {
@@ -47,5 +26,46 @@ new Vue({
                 break;
             }
         }
-    }
+    },
+    mounted(){
+        axios.get('./phpForConnect/memTravelBySelf.php') //根據哪個php
+        // axios.get(`./phpForConnect/memTravelBySelf.php?test=${test}`)
+
+        .then((res) => {
+            this.TravelBySelfList = res.data; //this.BBB = res.data;
+
+            console.log(res.data); //測試是否成功
+
+            // console.log(this.meetList);
+            for(let i = 0; i< this.TravelBySelfList.length; i++){  //動態生成內容，依據json有幾筆
+                this.TravelBySelfIndex.push(i)
+            }
+            // for(let i = 0; i< this.BBB.length; i++){  
+            //     this.CCC.push(i)
+            // }
+
+        })
+        .catch(error => {console.log(error)}); 
+    },
+    methods: {
+        Degree(value) {
+            switch(value){
+                case('1'):
+                    return '<div class="iconBox"><img src="./images/icons/icon_mountain.svg" alt="" class="iconDegree"></div><div class="iconBox"><img src="./images/icons/icon_mountain_a.svg" alt="" class="iconDegree"></div><div class="iconBox"><img src="./images/icons/icon_mountain_a.svg" alt="" class="iconDegree"></div><div class="iconBox"><img src="./images/icons/icon_mountain_a.svg" alt="" class="iconDegree"></div>';
+                break;
+                case('2'):
+                    return '<div class="iconBox"><img src="./images/icons/icon_mountain.svg" alt="" class="iconDegree"></div><div class="iconBox"><img src="./images/icons/icon_mountain.svg" alt="" class="iconDegree"></div><div class="iconBox"><img src="./images/icons/icon_mountain_a.svg" alt="" class="iconDegree"></div><div class="iconBox"><img src="./images/icons/icon_mountain_a.svg" alt="" class="iconDegree"></div>';
+                break;
+                case('3'):
+                    return '<div class="iconBox"><img src="./images/icons/icon_mountain.svg" alt="" class="iconDegree"></div><div class="iconBox"><img src="./images/icons/icon_mountain.svg" alt="" class="iconDegree"></div><div class="iconBox"><img src="./images/icons/icon_mountain.svg" alt="" class="iconDegree"></div><div class="iconBox"><img src="./images/icons/icon_mountain_a.svg" alt="" class="iconDegree"></div>';
+                break;
+                case('4'):
+                    return '<div class="iconBox"><img src="./images/icons/icon_mountain.svg" alt="" class="iconDegree"></div><div class="iconBox"><img src="./images/icons/icon_mountain.svg" alt="" class="iconDegree"></div><div class="iconBox"><img src="./images/icons/icon_mountain.svg" alt="" class="iconDegree"></div><div class="iconBox"><img src="./images/icons/icon_mountain.svg" alt="" class="iconDegree"></div>';
+                break;
+                default:
+                    return '應該很難';
+                break;
+            }
+        }
+    },
 });
