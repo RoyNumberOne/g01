7d92b7e93032042829cf4ecec58ad887d0eb7902
Hiko <toshihikomit@gmail.com>
2020-09-21 08:43:34 +0800

    0843

--- dev/phpForConnect/memTravelByOther.php
+++ dev/phpForConnect/memTravelByOther.php
@@ -7,7 +7,7 @@ try{
     session_start();
     $mem_no = $_SESSION['mem_no'];
 
-    $sql= "SELECT tp.tour_participate_mem, tp.tour_participate_tour, tp.tour_participate_situation, T.tour_no, T.tour_title,T.tour_innertext, T.tour_activitystart, M.mountain_image, M.mountain_area, M.degree_category, M.mountain_name
+    $sql= "SELECT tp.tour_participate_mem, tp.tour_participate_tour, tp.tour_participate_situation, T.tour_no, T.tour_title,T.tour_innertext, T.tour_activitystart, T.tour_activityend, T.tour_progress, T.tour_passed, M.mountain_image, M.mountain_area, M.degree_category, M.mountain_name
                 from tour_participate tp
                     join tour T on(tp.tour_participate_tour = T.tour_no)
                     join mountain M on(T.tour_mountain = M.mountain_no)
