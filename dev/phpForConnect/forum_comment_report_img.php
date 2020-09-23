b7f0cf2795cf1eaa7f6b2159a2626e3ab6474a4e
roycanfly <roylocanfly@gmail.com>
2020-09-23 17:22:19 +0800

    討論區留言、檢舉、即時更新及判斷是否檢舉過

--- dev/phpForConnect/forum_comment_report_img.php
+++ dev/phpForConnect/forum_comment_report_img.php
@@ -9,9 +9,9 @@ try{
     session_start();
     $comment_report_mem = $_SESSION['mem_no']; //誰檢舉
     // $comment_report_comment = $_REQUEST['comment_report_comment'];  //檢舉哪篇留言
-    $tour_post_no = $_REQUEST['tour_post_no']; //哪一團
+    $forum_post_no = $_REQUEST['forum_post_no']; //哪一團
     
-    $sql = "SELECT cp.comment_no , cr.comment_report_mem from comment_report cr right outer join comment_post cp on ( cr.comment_report_comment = cp.comment_no and cr.comment_report_mem = $comment_report_mem) where cp.tour_post_no = $tour_post_no;";
+    $sql = "SELECT cp.comment_no , cr.comment_report_mem from comment_report cr right outer join comment_post cp on ( cr.comment_report_comment = cp.comment_no and cr.comment_report_mem = $comment_report_mem) where cp.forum_post_no = $forum_post_no;";
     
 
     $pdoStatement = $pdo->prepare($sql);
